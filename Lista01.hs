06b9237b2297caedc4f324875adae5d806ec0fbc
Rodrigo Lima Barbosa12 <rodrigo.limabarbosa@upe.br>
2026-05-04 21:15:31 -0300

    Update Lista01.hs

--- Lista01.hs
+++ Lista01.hs
@@ -1,6 +1,3 @@
-import Data.List (group)
-import Data.Char (isDigit)
-
 -- I: Quantos Iguais
 quantosIguais :: Int -> Int -> Int -> Int
 quantosIguais a b c
@@ -9,10 +6,13 @@ quantosIguais a b c
     | otherwise = 0
 
 -- II: Maiores que a média
-quantosMaioresQueMedia :: Float -> Float -> Float -> Int
-quantosMaioresQueMedia a b c = 
-    let media = (a + b + c) / 3
-    in length [x | x <- [a, b, c], x > media]
+quantosMaiores :: Double -> Double -> Double -> Int
+quantosMaiores a b c = 
+    let
+    media = (a + b + c) / 3
+    contar x = if x > media then 1 else 0
+    in
+    contar a + contar b + contar c
 
 -- III e IV: Potências
 potencia_2 :: Int -> Int
@@ -23,16 +23,22 @@ potencia_4 x = potencia_2 (potencia_2 x)
 
 -- V: XOR (Ou exclusivo)
 xor :: Bool -> Bool -> Bool
-xor a b = a /= b
+xor a b = if a == b then False else True
 
 -- VI: Bhaskara
-x_maior :: Float -> Float -> Float -> Float
-x_maior a b c = ((-b) + delta) / (2 * a)
-    where delta = sqrt (b*b - 4*a*c)
+x_maior :: Double -> Double -> Double -> Double
+x_maior a b c
+    | delta >= 0 = (-b + sqrt delta) / (2*a)
+    | otherwise  = error "Nao ha raizes"
+    where
+    delta = b^2 - 4*a*c
 
-x_menor :: Float -> Float -> Float -> Float
-x_menor a b c = ((-b) - delta) / (2 * a)
-    where delta = sqrt (b*b - 4*a*c)
+x_menor :: Double -> Double -> Double -> Double
+x_menor a b c
+    | delta >= 0 = (-b - sqrt delta) / (2*a)
+    | otherwise  = error "Nao ha raizes"
+    where
+    delta = b^2 - 4*a*c
 
 -- VII: Somas
 somaInclui :: Int -> Int -> Int
@@ -46,85 +52,103 @@ multiplosNoIntervalo :: Int -> Int -> Int -> [Int]
 multiplosNoIntervalo n1 n2 n3 = [x | x <- [n1..n2], x `mod` n3 == 0]
 
 -- IX: Multiplicar sem *
-multiplicar :: Int -> Int -> Int
-multiplicar a b
-    | b == 0    = 0
-    | b > 0     = sum (replicate b a)
-    | otherwise = negate (sum (replicate (abs b) a))
+multiplica :: Int -> Int -> Int
+multiplica a b
+    | a == b || b == 0    = 0
+    | a > 0               = sum (replicate b a)
+    | otherwise           = negate (sum (replicate (abs a) b))
 
 -- X: Modulo manual
 mod2 :: Int -> Int -> Int
 mod2 a b
+    | b == 0    = error "Divisao por zero"
+    | a < 0     = mod2 (a + abs b) b
     | a < b     = a
     | otherwise = mod2 (a - b) b
 
 -- XI: Sequência de Raízes
 seqA :: Int -> Double
-seqA 1 = sqrt 6
-seqA n = sqrt (6 + seqA (n - 1))
+seqA n
+    | n <= 1    = sqrt 6
+    | otherwise = sqrt (6 + seqA (n-1))
 
 -- XII: Fatorial e Combinação
 fatorial :: Int -> Int
 fatorial 0 = 1
 fatorial n = n * fatorial (n - 1)
 
-combinar :: Int -> Int -> Int
-combinar m n = fatorial m `div` (fatorial n * fatorial (m - n))
+combinacao :: Int -> Int -> Int
+combinacao m n
+        | m < n     = 0
+        | otherwise = fatorial m `div` (fatorial n * fatorial (m - n))
 
 -- XIII: Maior e seu Índice
-maiorP :: [Int] -> (Int, Int)
-maiorP [] = error "Lista vazia"
-maiorP xs = foldl1 (\(m, pm) (x, px) -> if x > m then (x, px) else (m, pm)) (zip xs [0..])
+maiorEIndice :: [Int] -> (Int, Int)
+maiorEIndice [] = error "Lista vazia"
+maiorEIndice (x:xs) = auxiliar xs x 0 1
+    where
+        auxiliar [] maior idxMaior _ = (maior, idxMaior)
+        auxiliar (y:ys) maior idxMaior idxAtual
+            | y > maior = auxiliar ys y idxAtual (idxAtual + 1)
+            | otherwise = auxiliar ys maior idxMaior (idxAtual + 1)
 
 -- XIV: Tradução de dígitos
-dic_10 :: [(Int, String)]
-dic_10 = [(0, "zero"), (1, "um"), (2, "dois"), (3, "tres"), (4, "quatro"), 
-          (5, "cinco"), (6, "seis"), (7, "sete"), (8, "oito"), (9, "nove")]
-
 traduz :: [Int] -> [String]
-traduz xs = [v | x <- xs, (k, v) <- dic_10, x == k]
+traduz xs = [v | x <- xs, (k,v) <- dic_10, x ==k]
+    where
+    dic_10 = [(0, "zero"), (1, "um"), (2, "dois"), (3, "tres"), (4, "quatro"), (5, "cinco"),
+          (6, "seis"), (7, "sete"), (8, "oito"), (9, "nove")]
 
 -- XV: Deletar posição N
-delPosicaoN :: [Int] -> Int -> [Int]
-delPosicaoN xs n = take n xs ++ drop (n + 1) xs
+delPosicao :: [Int] -> Int -> [Int]
+delPosicao [] _ = []
+delPosicao (x:xs) 0 = xs
+delPosicao (x:xs) n = x : delPosicao xs (n-1)
 
 -- XVI: Inserir na posição X
 inserirPosicaoX :: [Int] -> Int -> Int -> [Int]
-inserirPosicaoX xs n elemento = take n xs ++ [elemento] ++ drop n xs
+inserirPosicaoX [] valor _     = [valor]
+inserirPosicaoX lista valor 0  = valor : teste
+inserirPosicaoX (x:xs) valor n = x : inserirPosicaoX xs valor (n-1)
 
 -- XVII: Elemento na posição N
-elementoNaPosicao :: [a] -> Int -> a
-elementoNaPosicao [] _ = error "Index out of bounds"
-elementoNaPosicao (x:xs) n
-    | n == 0    = x
-    | otherwise = elementoNaPosicao xs (n - 1)
+elemP :: [Int] -> Int -> Int
+elemP [] _ = error "Indice fora de alcance da lista"
+elemP (x:xs) 0 = x
+elemP (x:xs) n = elemP xs (n-1)
 
 -- XVIII: Merge (Fusão de listas ordenadas)
 merge :: [Int] -> [Int] -> [Int]
-merge [] ys = ys
-merge xs [] = xs
+merge [] lista2 = lista2
+merge lista1 [] = lista1
 merge (x:xs) (y:ys)
     | x <= y    = x : merge xs (y:ys)
     | otherwise = y : merge (x:xs) ys
 
 -- XIX: Interseção
 intersecao :: [Int] -> [Int] -> [Int]
-intersecao xs ys = [x | x <- xs, x `elem` ys]
+intersecao lista1 lista2 = [x | x <- lista1, x `elem` lista2]
 
 -- XX: Comprimir
 comprime :: String -> String
-comprime s = concatMap format (group s)
-  where
-    format g | length g > 3 = "!" ++ show (length g) ++ [head g]
-             | otherwise    = g
+comprime [] = []
+comprime s = 
+    let (grupo, resto) = span (== head s) s
+         n = length grupo
+         caractere = head grupo
+    in if n > 3
+       then "!" ++ show n ++ [caractere] ++ comprime resto
+       else grupo ++ comprime resto
 
 -- XXI: Descomprimir
 descomprime :: String -> String
 descomprime [] = []
-descomprime ('!':xs) = 
-    let numStr = takeWhile isDigit xs
-        resto  = dropWhile isDigit xs
-        n      = read numStr
-        char   = head resto
-    in replicate n char ++ descomprime (tail resto)
+descomprime ('!':resto) = 
+    let (numStr, sobra) = span eDigito resto
+         n = read numStr :: Int
+         caractere = head sobra
+         cauda = tail sobra
+    in replicate n caractere ++ descomprime cauda
+where
+  eDigito c = c >= '0' && c c = '9'
 descomprime (x:xs) = x : descomprime xs
